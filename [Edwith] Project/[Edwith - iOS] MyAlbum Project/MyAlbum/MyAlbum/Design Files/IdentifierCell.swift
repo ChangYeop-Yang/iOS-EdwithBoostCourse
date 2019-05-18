a9a344010b4b6f8c66b90cd27cf6bf037f7dd2b2
ChangYeop-Yang <yeop9657@naver.com>
2019-05-19 00:02:32 +0900

    [#5] Create 2019.05.19. 00:02:28
    [#5] Create 2019.05.19. 00:02:28

---
+++ [Edwith] Project/[Edwith - iOS] MyAlbum Project/MyAlbum/MyAlbum/Design Files/IdentifierCell.swift
@@ -0,0 +1,14 @@
+//
+//  IdentifierCell.swift
+//  MyAlbum
+//
+//  Created by 양창엽 on 18/05/2019.
+//  Copyright © 2019 양창엽. All rights reserved.
+//
+
+import Foundation
+
+internal enum IndentifierCell: String {
+    case detailAlbumCell    = "DetailAlbumCell"
+    case repersentAlbumCell = "RepresentAlbumCell"
+}
